edb09d3cedd409242616696d65910d55ec469a68
bhuvaneshchennai01-oss <bhuvaneshchennai01@gmail.com>
2025-10-08 13:54:42 +0530

    python

---
+++ leap.py
@@ -0,0 +1,9 @@
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+       print("Leap year")
+    else:
+        print("Not")
+
+is_leap_year(2024) 
+is_leap_year(2025)       
+
